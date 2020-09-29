8b286f6460a19d67d47e30bd8ca96dd212d06336
AtharvaCM <atharvacm@kali.net>
2020-09-29 10:41:22 +0530

    heroku changes

--- freshfromfarm/settings.py
+++ freshfromfarm/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 
@@ -143,9 +143,9 @@ STATICFILES_FINDERS = {
 }
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 # Media Root
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
